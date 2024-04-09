e855b44b29e11f99d9cce6250f4f245de0d3153a
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-04-09 23:19:10 +0300

    ts hw 02 ready to check

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface PageTitle {
+  title: string;
+} 
+
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<PageTitle> {
+  pageInfo(): void {
     console.log(this.props.title);
   }
 }
